dfde8ff68b37c1914402d3f4fc68c2267788f719
Laksh K <laksh-51.lakshkhobragade51@gmail.com>
2025-07-09 17:09:06 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,11 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
+from sklearn.model_selection import train_test_split
+
 
 def save_object(file_path, obj):
     try:
@@ -19,6 +22,49 @@ def save_object(file_path, obj):
             
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models: dict):
+    try:
+        report = {}
+
+        for name, model in models.items():
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[name] = test_model_score  # Or you can store both train/test if needed
+
+        return report    
+    
+# def evaluate_models(X_train, y_train, X_test,y_test, models):
+#     try:
+#         X_train, X_test, y_train, y_test = train_test_split(
+#             X, y, test_size=0.2, random_state=42
+#         )
         
+#         report ={}
               
+#         for i in range(len(list(models))):
+#                   model = list(models.values())[i]
+                  
+#                   model.fit(X_train, y_train)
+                  
+#                   y_train_pred = model.predict(X_train)
+                  
+#                   y_test_pred = model.predict(X_test)
+                  
+#                   train_model_score = r2_score(y_train, y_train_pred)
+                  
+#                   test_model_score = r2_score(y_test, y_test_pred)
+                  
+#                   report[list(models.keys())[i]] = test_model_score
+                  
+#         return report
+        
+    except Exception as e:
+        raise CustomException      
               
